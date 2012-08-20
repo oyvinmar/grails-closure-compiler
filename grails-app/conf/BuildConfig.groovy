ff5baef66757dcf84e3c91695e215c107f90ed75
Øyvind Marthinsen <oyvinmar@gmail.com>
2012-08-20 13:16:20 +0200

    First working sample. Compiles js files with the SIMPLE_OPTIMIZATION option.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,19 +13,11 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenCentral()
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        compile 'com.google.javascript:closure-compiler:r1918'
     }
 
     plugins {
---
+++ grails-app/resourceMappers/org/grails/plugin/resources/GoogleClosureCompilerResourceMapper.groovy
@@ -0,0 +1,48 @@
+import org.grails.plugin.resource.mapper.MapperPhase
+import com.google.javascript.jscomp.Compiler
+import com.google.javascript.jscomp.CompilerOptions
+import com.google.javascript.jscomp.JSSourceFile
+import com.google.javascript.jscomp.CompilationLevel
+import com.google.javascript.jscomp.Result
+import org.grails.plugin.resource.JavaScriptBundleResourceMeta
+
+class GoogleClosureCompilerResourceMapper {
+    def phase = MapperPhase.COMPRESSION
+
+    static defaultExcludes = ['**/*.min.js']
+    static defaultIncludes = ['**/*.js']
+
+    def map(resource, config) {
+        if (resource instanceof JavaScriptBundleResourceMeta) {
+            return false
+        }
+
+        JSSourceFile input = JSSourceFile.fromFile(resource.processedFile)
+        JSSourceFile extern = JSSourceFile.fromCode("/dev/null", "")
+
+        CompilerOptions options = new CompilerOptions();
+        CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
+
+        try {
+            Compiler compiler = new Compiler();
+            Result result = compiler.compile(extern,input,options);
+            proccessResult(result)
+            resource.processedFile.write(compiler.toSource())
+        } catch (e) {
+            log.error "Exception when compiling ${resource.processedFile.name} with Google Closure: $e.message"
+        }
+    }
+
+   def proccessResult(Result result) {
+       //Log and return false on errors
+       if (result.errors) {
+           result.errors.each {log.error(it.toString())}
+           return false
+       }
+
+       //Log warnings
+       if (result.warnings) {
+           result.warnings.each {log.warn(it.toString())}
+       }
+    }
+}
