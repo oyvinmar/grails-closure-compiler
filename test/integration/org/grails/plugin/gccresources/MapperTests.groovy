331c3801c04f0f7b1baff74ff684e0d839254060
Øyvind Marthinsen <oyvinmar@gmail.com>
2012-10-04 21:26:38 +0200

    Fixed integration test

--- test/integration/org/grails/plugin/gccresources/MapperTests.groovy
+++ test/integration/org/grails/plugin/gccresources/MapperTests.groovy
@@ -7,11 +7,16 @@ class MapperTests extends GrailsUnitTestCase {
 
     void testJSMinifying() {
         def r = new ResourceMeta()
-        r.processedFile =  new File(System.getProperty("user.dir") + '/web-app/js/jquery-1.8.1.js')
+        def filePath = System.getProperty("user.dir") + '/web-app/js/jquery-1.8.2.js'
+        r.processedFile = new File(filePath + '.copy.js') << new File(filePath).text
         def before = r.processedFile.size()
+
         GoogleClosureCompilerResourceMapper.newInstance().with {
             map(r, new ConfigObject())
         }
+
         assertTrue(r.processedFile.size() < before)
+
+        r.processedFile.delete()
     }
 }
