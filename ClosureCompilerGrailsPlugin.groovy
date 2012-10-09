22c745aad54d49be759b337dd18e2f283e8a0579
Øyvind Marthinsen <oyvinmar@gmail.com>
2012-10-09 12:01:07 +0200

    Updated plugin info and dependencies

--- ClosureCompilerGrailsPlugin.groovy
+++ ClosureCompilerGrailsPlugin.groovy
@@ -1,10 +1,12 @@
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
 class ClosureCompilerGrailsPlugin {
     // the plugin version
-    def version = "0.3"
+    def version = "0.4"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [resources:'1.0.RC3 > *']
+    def dependsOn = [resources:'1.0 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
@@ -37,39 +39,15 @@ N.B. It builds/depends on the Grails Resources Plugin.
     //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-    //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "Github", url: "https://github.com/oyvinmar/grails-closure-compiler/issues" ]
 
     // Online location of the plugin's browseable source code.
-    //    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
+    def scm = [ url: "https://github.com/oyvinmar/grails-closure-compiler" ]
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
+        def config = ConfigurationHolder.getConfig()
+        if (!config.closurecompiler.compilation_level) {
+            config.closurecompiler.compilation_level= 'SIMPLE_OPTIMIZATIONS'
+        }
     }
 }
