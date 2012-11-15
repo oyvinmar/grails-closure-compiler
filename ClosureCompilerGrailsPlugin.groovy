ba7086f5e5fbab1cc9bbaa68df1685acfbdad0ac
Øyvind Marthinsen <oyvinmar@gmail.com>
2012-11-15 12:37:55 +0100

    Updated version number

--- ClosureCompilerGrailsPlugin.groovy
+++ ClosureCompilerGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class ClosureCompilerGrailsPlugin {
     // the plugin version
-    def version = "0.9"
+    def version = "0.9.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
